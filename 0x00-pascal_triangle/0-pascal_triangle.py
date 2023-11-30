b0a7f951ae2dc6726362f70d0cbf47f170dc4cca
Famouus Bernard <famousbernard@gmail.com>
2023-11-30 13:17:45 -0800

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,22 +2,26 @@
 """Pascal triangle interview"""
 
 def pascal_triangle(n):
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
     if n <= 0:
         return []
-    
+
     triangle = [0] * n
-    
+
     for i in range(n):
-        row = [0] * (i+1)
-        row[0] = 1
-        row[len(row) - 1] = 1
-        
+        new_row = [0] * (i+1)
+        new_row[0] = 1
+        new_row[len(new_row) - 1] = 1
+
         for j in range(1, i):
-            if j > 0 and j < len(row):
+            if j > 0 and j < len(new_row):
                 a = triangle[i - 1][j]
                 b = triangle[i - 1][j - 1]
-                row[j] = a + b
-        
-        triangle[i] = row
-    
+                new_row[j] = a + b
+
+        triangle[i] = new_row
+
     return triangle
+

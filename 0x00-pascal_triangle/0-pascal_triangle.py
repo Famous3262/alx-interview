0fe9fc7698c76c11e3311d38486292491ec681c2
Famouus Bernard <famousbernard@gmail.com>
2023-11-30 13:39:36 -0800

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Pascal triangle interview"""
+"""Pascal Triangle Interview """
+
 
 def pascal_triangle(n):
     """
@@ -8,20 +9,20 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [0] * n
+    pascal_triangle = [0] * n
 
     for i in range(n):
+        # define a row and fill first and last idx with 1
         new_row = [0] * (i+1)
         new_row[0] = 1
         new_row[len(new_row) - 1] = 1
 
         for j in range(1, i):
             if j > 0 and j < len(new_row):
-                a = triangle[i - 1][j]
-                b = triangle[i - 1][j - 1]
+                a = pascal_triangle[i - 1][j]
+                b = pascal_triangle[i - 1][j - 1]
                 new_row[j] = a + b
 
-        triangle[i] = new_row
+        pascal_triangle[i] = new_row
 
-    return triangle
-
+    return pascal_triangle

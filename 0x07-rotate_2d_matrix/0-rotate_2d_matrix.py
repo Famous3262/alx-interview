c0f8fc4015eaade073221903c195a4255baa694b
Famouus Bernard <famousbernard@gmail.com>
2024-01-26 17:48:47 -0800

    rotate 2d matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" A function that implements an n x n 2D matrix
+and rotate it 90 degrees clockwise
+"""
+
+
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+    # Transpose the matrix
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    # Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
